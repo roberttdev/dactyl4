a599a2dfd0df429e5c70266221150977c577ccf7
roberttdev <roberttdev@gmail.com>
2014-05-16 15:53:51 -0400

    Added DACTYL SMTP config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,9 +64,18 @@ DC::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+      address:                'smtp.gmail.com',
+      port:                   587,
+      domain:                 'denney.ws',
+      user_name:              'dactyl@denney.ws',
+      password:               'z[FL7DGm.6sw',
+      authentication:         'plain',
+      enable_starttls_auto:   true
+  }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
--- config/initializers/smtp_configuration.rb
+++ config/initializers/smtp_configuration.rb
@@ -1,13 +1,1 @@
-unless Rails.env.development?
 
-  ActionMailer::Base.smtp_settings = {
-    :address              => DC::SECRETS['smtp_host'],
-    :port                 => 25,
-    :domain               => DC::SECRETS['smtp_domain'],
-    :user_name            => DC::SECRETS['smtp_user'],
-    :password             => DC::SECRETS['smtp_password'],
-    :authentication       => :login,
-    :enable_starttls_auto => true
-  }
-
-end
