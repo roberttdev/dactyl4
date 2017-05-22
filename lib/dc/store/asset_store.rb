714f160660ee3b78579de24e40493b5b9b342ad4
Rob Williams <roberttdev@gmail.com>
2017-05-22 15:43:47 -0400

    Replace S3 with local file storage in staging/prod

--- lib/dc/store/asset_store.rb
+++ lib/dc/store/asset_store.rb
@@ -8,13 +8,13 @@ module DC
     # The AssetStore is responsible for storing search-opaque document assets,
     # either on S3 (or in development on the local filesystem in a tmp dir).
     class AssetStore
-      if Rails.env.development?
+ #     if Rails.env.development?
         include FileSystemStore
         extend FileSystemStore::ClassMethods
-      else
-        include AwsS3Store
-        extend  AwsS3Store::ClassMethods
-      end
+#    else
+#        include AwsS3Store
+#        extend  AwsS3Store::ClassMethods
+#      end
     end
 
   end
